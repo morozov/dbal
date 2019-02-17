3b23999cfd413d303db7e366606d675738113413
Sergei Morozov <morozov@tut.by>
2019-02-17 15:36:19 -0800

    PHPStan now passes (but PHPUnit doesn't)

--- lib/Doctrine/DBAL/Platforms/PostgreSQL100Platform.php
+++ lib/Doctrine/DBAL/Platforms/PostgreSQL100Platform.php
@@ -21,12 +21,18 @@ class PostgreSQL100Platform extends PostgreSQL94Platform
 
     public function getListSequencesSQL(?string $database) : string
     {
+        if ($database !== null) {
+            $catalogExpression = $this->quoteStringLiteral($database);
+        } else {
+            $catalogExpression = '(SELECT current_catalog)';
+        }
+
         return 'SELECT sequence_name AS relname,
                        sequence_schema AS schemaname,
                        minimum_value AS min_value, 
                        increment AS increment_by
                 FROM   information_schema.sequences
-                WHERE  sequence_catalog = ' . $this->quoteStringLiteral($database) . "
+                WHERE  sequence_catalog = ' . $catalogExpression . "
                 AND    sequence_schema NOT LIKE 'pg\_%'
                 AND    sequence_schema != 'information_schema'";
     }
